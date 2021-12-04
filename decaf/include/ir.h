e1aee72296b1e9482dc888468d0471dcaf5277af
zeFresk <8807862+zeFresk@users.noreply.github.com>
2021-12-04 15:58:59 +0100

    ir.h

---
+++ decaf/include/ir.h
@@ -0,0 +1,22 @@
+/**
+ * @file
+ * @author Léo Chéneau <leo.cheneau@etu.unistra.fr
+ * @version 1.0
+ * @brief Regroupement des fonctions liées au code intermédiaire
+ *
+ * @defgroup IR IR
+ * @brief Code intermédiaire
+ *
+ */
+#ifndef INCLUDE__IR__H
+#define INCLUDE__IR__H
+/** @addtogroup IR
+ * @{
+ */
+
+#include "quadops.h"
+#include "gencode.h"
+#include "incomplete.h"
+
+///@}
+#endif
