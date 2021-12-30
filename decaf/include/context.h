b894ddd10681bfb76142e20429f718196c385653
CHENEAU LEO <leo.cheneau@etu.unistra.fr>
2021-12-30 21:33:22 +0000

    Headers et déclarations des fonctions/structures nécessaires au parsing

---
+++ decaf/include/context.h
@@ -0,0 +1,128 @@
+/**
+ * @file
+ * @author Léo Chéneau <leo.cheneau@etu.unistra.fr
+ * @version 1.0
+ * @brief Tables des symboles
+ * @ingroup CTX
+ */
+#ifndef INCLUDE__CTX_CONTEXT__H
+#define INCLUDE__CTX_CONTEXT__H
+
+#include "entry.h"
+
+/**
+ * @addtogroup CTX
+ * @{
+ *
+ * @defgroup Context Context
+ * @brief Tables des symboles
+
+ * @addtogroup Context
+ * @{
+ */
+
+/**
+ * @brief Nombre d'entrées par contexte sans extensions
+ */
+#define DEF_ENTRIES_PER_CTX 64
+
+/**
+ * @struct context
+ * @brief Structure représentant une table des symboles
+ * @warning à ne pas manipuler directement
+ *
+ * Cette structure contient les entrées dans la table
+ */
+struct context {
+	struct entry entries[DEF_ENTRIES_PER_CTX]; ///< Entrées internes
+	size_t used; ///< Nombre d'entrées utilisées en interne
+	const struct context* parent; ///< Context parent, NULL si racine
+	struct context* next; ///< Context de même niveau stockant plus d'entrées
+};
+
+
+extern struct context racine;
+
+
+
+/**
+ * @brief Empile un nouveau contexte vierge
+ *
+ * @return Le :context empilé
+ */
+struct context* ctx_pushctx();
+
+/**
+ * @brief Dépile le dernier contexte empilé
+ *
+ * @return Le nouveau sommet de pile
+ */
+struct context* ctx_popctx();
+
+/**
+ * @brief Récupère le contexte courant, sommet de pile
+ *
+ * @return Le sommet de la pile des contextes
+ */
+struct context* ctx_currentctx();
+
+/**
+ * @brief Ajoute une entrée dans le contexte courant
+ *
+ * @param[in] id Identifiant de l'entrée
+ *
+ * @return L'entrée ajoutée au contexte
+ *
+ * @warning Ne pas oublier de renseigner les autres informations sur l'entrée plus tard !
+ */
+struct entry* ctx_newname(const char id[MAX_IDENTIFIER_SIZE]);
+
+/**
+ * @brief Crée un entrée dans le contexte courant
+ *
+ * @return Une entrée valide et unique
+ *
+ * @note L'entrée n'est PAS supprimée automatiquement
+ *
+ * Les identifiants sont générés automatiquements et sont uniques.
+ */
+struct entry* ctx_make_temp();
+
+/**
+ * @brief Recherche un identifiant depuis la table des symoles courante
+ *
+ * @param id Identifiant à rechercher
+ *
+ * @return NULL si l'entrée n'existe pas, l'entrée sinon.
+ *
+ * @warning Ne pas oublier de tester la valeur de retour de cette fonction
+ */
+struct entry* ctx_lookup(const char id[MAX_IDENTIFIER_SIZE]);
+
+/**
+ * @brief Compte le nombre d'entrées dans le contexte demandé
+ *
+ * @param ctx Contexte à examiner
+ *
+ * @return Le nombre d'entrée
+ *
+ * @warning N'examine pas les fils
+ */
+size_t ctx_count_entries(const struct context* ctx);
+
+/**
+ * @brief Compte le nombre d'octets à allouer pour le contexte demandé
+ *
+ * @param ctx Contexte à examiner
+ *
+ * @return Le nombbre d'octets à allouer
+ *
+ * @warning N'examine pas les fils
+ */
+size_t ctx_count_bytes(const struct context* ctx);
+
+/**
+ * @}
+ * @}
+ */
+#endif
---
+++ decaf/include/optim.h
@@ -0,0 +1,25 @@
+/**
+ * @file optim.h
+ * @author 
+ * @brief Fichier regroupant tout ce qui à trait à l’optimisation
+ * @version 0.1
+ *
+ * @defgroup Optimisation Optimisation
+ * @brief Optimisations
+ *
+ */
+#ifndef INCLUDE__OPTIM__H
+#define INCLUDE__OPTIM__H
+
+/**
+ * @addtogroup Optimisation
+ * @{
+ */
+
+#include "opticode.h"
+
+/**
+ * @}
+ */
+
+#endif
