d134548a07519aad9f00d742d5286a1bfb6dde8b
CHENEAU LEO <leo.cheneau@etu.unistra.fr>
2022-01-05 14:05:58 +0000

    Gencode

---
+++ decaf/src/quadops.c
@@ -0,0 +1,86 @@
+#include "quadops.h"
+#include "entry.h"
+
+#include <assert.h>
+
+#define NUM_QOP_BIN 5
+// quads arithmétique
+int is_arith(enum Q_OP qop)
+{
+	static enum Q_OP arith_quads[NUM_QOP_BIN] = { Q_ADD, Q_SUB, Q_MUL, Q_DIV, Q_MOD };
+	for (int i = 0; i < NUM_QOP_BIN; ++i) {
+		if (qop == arith_quads[i])
+			return 1;
+	}
+	return 0;
+}
+
+struct quad quad_arith(const struct entry* res, const struct entry* lhs, enum Q_OP qop, const struct entry* rhs)
+{
+	assert(is_arith(qop) && "Expected binary operator");
+	assert(res && "quad_arith expecting NON null result entry");
+	assert(lhs && "quad_arith expecting NON null lhs entry");
+	assert(rhs && "quad_arith expecting NON null rhs entry");
+	struct quad q = {.op = qop, .res = res, .lhs = lhs, .rhs = rhs};
+	return q;
+}
+
+struct quad quad_aff(const struct entry* res, const struct entry* val)
+{
+	assert(res && "quad_aff expecting NON null result entry");
+	assert(val && "quad_aff expecting NON null value entry");
+	struct quad q = {.op = Q_AFF, .res = res, .lhs = val};
+	return q;
+}
+
+struct quad quad_neg(const struct entry* res, const struct entry* val)
+{
+	assert(res && "quad_neg expecting NON null result entry");
+	assert(val && "quad_neg expecting NON null value entry");
+	struct quad q = {.op = Q_NEG, .res = res, .lhs = val};
+	return q;
+
+}
+
+struct quad quad_goto(quad_id_t qid)
+{
+	struct quad q = {.op = Q_GOT, .dst = qid};
+	return q;
+}
+
+struct quad quad_ifgoto(const struct entry* lhs, enum CMP_OP cmp_op, const struct entry* rhs, quad_id_t qid)
+{
+	assert(lhs && "quad_ifgoto expecting NON null lhs entry");
+	assert(rhs && "quad_ifgoto expecting NON null rhs entry");
+	struct quad q = {.op = Q_IFG, .lhs = lhs, .cmp = cmp_op, .rhs = rhs, .dst = qid};
+	return q;
+}
+
+struct quad quad_param(const struct entry* var)
+{
+	assert(var && "quad_param expecting NON null var entry");
+	struct quad q = {.op = Q_PAR, .lhs = var};
+	return q;
+}
+
+struct quad quad_call(const struct entry* res, const struct entry* fct)
+{
+	assert(res && "quad_call expecting NON null result entry");
+	assert(fct && "quad_call expecting NON null function entry");
+	struct quad q = {.op = Q_CAL, .res = res, .lhs = fct};
+	return q;
+}
+
+struct quad quad_proc(const struct entry* proc)
+{
+	assert(proc && "quad_proc expecting NON null procedure entry");
+	struct quad q = {.op = Q_PRO, .lhs = proc};
+	return q;
+}
+
+struct quad quad_cst(const struct entry* var, int cst)
+{
+	assert(var && "quad_cst expecting NON null var entry");
+	struct quad q = {.op = Q_CST, .res = var, .val = cst};
+	return q;
+}
---
+++ decaf/test/unitary/gencode_basic.c
@@ -0,0 +1,130 @@
+/*
+ * Tests très simples de gencode
+ */
+
+#include "test_suite.h"
+
+#include "gencode.h"
+#include "quadops.h"
+#include "context.h"
+#include "genasm.h"
+#include "typelist.h"
+
+#include <stdlib.h>
+#include <string.h>
+
+extern size_t co_used;
+extern size_t next;
+extern size_t used;
+extern struct context* sommet;
+
+char* tokenize(const char* str)
+{
+	static char buf[MAX_IDENTIFIER_SIZE];
+	int n = snprintf(buf, MAX_IDENTIFIER_SIZE, "%s", str);
+	ASSERT_TRUE(n > 0 && n < MAX_IDENTIFIER_SIZE);
+	return buf;
+}
+
+int setup(void** data)
+{
+	(void)(data);
+
+	co_used = 0;
+	used = 0;
+	next = 0;
+	sommet = NULL;
+
+	ctx_pushctx(); // super-global
+	struct typelist* one_int = typelist_new();
+	typelist_append(one_int, BT_INT);
+	ctx_newname(tokenize("WriteInt"))->type = typedesc_make_function(BT_INT, one_int);
+
+	ctx_pushctx(); // root
+	ctx_newname(tokenize("global"))->type = typedesc_make_var(BT_BOOL);
+	struct typelist* two_int = typelist_new();
+	typelist_append(typelist_append(two_int, BT_INT), BT_INT);
+	ctx_newname(tokenize("main"))->type = typedesc_make_function(BT_INT, two_int);
+
+	ctx_pushctx(); // main args
+	ctx_newname(tokenize("argc"))->type = typedesc_make_var(BT_INT);
+	ctx_newname(tokenize("argv"))->type = typedesc_make_var(BT_INT);
+
+	ctx_pushctx(); // contenu du main
+
+	return 1;
+}
+
+int teardown(void** data)
+{
+	(void)(data);
+
+	co_used = 0;
+	used = 0;
+	next = 0;
+	sommet = NULL;
+
+	return 1;
+}
+
+int all_quads_empty(void* data)
+{
+	(void)data;
+	size_t sz = 0;
+	struct quad* qs = get_all_quads(&sz);
+	ASSERT_EQ(sz, 0);
+	ASSERT_TRUE(qs != NULL);
+	return 1;
+}
+
+int nq_empty(void* data)
+{
+	(void)(data);
+	ASSERT_EQ(nextquad(), 0);
+	return 1;
+}
+
+int gq_invalid(void* data)
+{
+	(void)data;
+	getquad(0);
+	return 0;
+}
+
+int gq_ok(void* data)
+{
+	(void)(data);
+
+	struct quad q = quad_goto(0);
+	gencode(q);
+
+	ASSERT_EQ(getquad(0)->dst, q.dst);
+	ASSERT_EQ(getquad(0)->op, q.op);
+	return 1;
+}
+
+#define TOO_MUCH_QUADS 1000000
+int gc_full(void* data)
+{
+	(void)(data);
+
+	struct entry* res = ctx_newname(tokenize("res"));
+	for (size_t i = 0; i < TOO_MUCH_QUADS; ++i) {
+		gencode(quad_cst(res, 42));
+	}
+	return 0;
+}
+
+int main()
+{
+	struct test_suite gc;
+
+	gc = make_ts("Gencode basic tesrs", setup, teardown);
+	add_test(&gc, all_quads_empty, "get_all_quads works if empty");
+	add_test(&gc, nq_empty, "nextquad works if empty");
+	add_test_assert(&gc, gq_invalid, "getquad triggers assert for invalid qid");
+	add_test(&gc, gq_ok, "getquad retrieves correct quad");
+	add_test_assert(&gc, gc_full, "There is a limit to quad generation");
+
+	return exec_ts(&gc) ? EXIT_SUCCESS : EXIT_FAILURE;
+}
