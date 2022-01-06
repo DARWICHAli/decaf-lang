50f2686c430f3615aa01fc1abe0fdca3e6176e3f
DARWICH ALI <adarwich@etu.unistra.fr>
2022-01-06 17:03:49 +0100

    incomplete

---
+++ decaf/src/incomplete.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include "incomplete.h"
+#include "gencode.h"
+#include "quadops.h"
+#include <assert.h>
+#include <string.h>
+
+struct quad_list qlist_new()
+{
+    struct quad_list qlst;
+    memset(qlst.quads,0,sizeof(quad_id_t)*QUADLIST_MAX_SIZE);
+	qlst.used = 0;
+    return qlst;
+}
+
+
+
+struct quad_list* qlist_append(struct quad_list* qlst, quad_id_t qid)
+{
+    assert(qlst->used+1 < QUADLIST_MAX_SIZE && "Max Reached!");
+    assert(getquad(qid) && "quad n'existe pas!");
+    qlst->quads[qlst->used++] = qid;
+	return qlst;
+}
+
+void qlist_complete(struct quad_list* qlst, quad_id_t qid)
+{
+    for (size_t i = 0; i < qlst->used; i++) {
+        if((getquad(qlst->quads[i])->op == Q_IFG) && !(getquad(qlst->quads[i])->res) ){
+            getquad(qlst->quads[i])->dst = qid;
+        }
+    }
+    return;
+}
